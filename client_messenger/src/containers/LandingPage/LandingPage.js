513cd20c2e268ac5ec8a032f93b1563122e63253
Patryk <patryk744@op.pl>
2020-08-11 22:20:32 +0200

    Login logic in client app

--- client_messenger/src/containers/LandingPage/LandingPage.js
+++ client_messenger/src/containers/LandingPage/LandingPage.js
@@ -2,12 +2,34 @@ import React,{useState} from 'react';
 import classes from './LandingPage.module.css';
 import Navigation from '../../components/Navigation/Navigation';
 import {Form,Button,Image} from 'react-bootstrap';
+import axios from 'axios';
 
 const LandingPage = props=>{
     const [state,setState]= useState({
         login: '',
         password: ''
     });
+
+    const inputHandler = event =>{
+      setState({
+        ...state,
+        [event.target.name]: event.target.value
+      });
+    }
+    
+    const login = ()=>{
+      axios.post('http://localhost:3000/auth/login',{
+        login: state.login,
+        password: state.password
+      })
+      .then(response=>{
+        console.log(response); 
+      })
+      .catch(err=>{
+        console.log(err);
+      })
+    }
+
     return (
         <>
         <Navigation home/>
@@ -15,12 +37,12 @@ const LandingPage = props=>{
       <div className={classes.LandingPage}>
         <Form style={{marginTop: '25px'}}>
           <Form.Group controlId="formBasicEmail">
-            <Form.Control type="email" placeholder="Enter email" className={classes.input} style={{width: '50%'}}/>
+            <Form.Control type="text" placeholder="Enter Login" className={classes.input} style={{width: '50%'}} name="login" onChange={inputHandler} value={state.login}/>
           </Form.Group>
           <Form.Group controlId="formBasicLogin">
-            <Form.Control type="text" placeholder="Enter login" className={classes.input} style={{width: '50%'}}/>
+            <Form.Control type="text" placeholder="Enter Password" className={classes.input} style={{width: '50%'}} name="password" onChange={inputHandler} value={state.password}/>
           </Form.Group>
-          <Button className={classes.button}>
+          <Button className={classes.button} onClick={login}>
             Login
          </Button>
         </Form>
